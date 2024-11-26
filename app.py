746522dee3ae0bff76cf63db2a32be626abd34af
ingAbrahamVillalobos <ing.abrahampv@gmail.com>
2024-11-25 21:23:21 -0600

    Revert project to original state

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 import streamlit as st
-import os
-
-# Configura el puerto asignado dinámicamente por Render
-port = int(os.environ.get("PORT", 8501))
-
-# Configura la página principal (opcional, para evitar advertencias de Streamlit)
-st.set_page_config(page_title="Lanzar una moneda", layout="wide")
 
 st.header('Lanzar una moneda')
 
